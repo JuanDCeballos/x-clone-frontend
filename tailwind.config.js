ba14f9adb67d9739688700ca19bffc6eb05ff803
Juan Ceballos <juandavi789@hotmail.com>
2024-09-04 23:55:48 -0500

    Add custom fontSize

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontSize: {
+      custom: ['20px', '24px'],
+    },
     extend: {},
   },
   plugins: [],
