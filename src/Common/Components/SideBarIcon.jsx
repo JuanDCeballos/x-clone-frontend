fc1de75b44ab4008b0a6527a40df4141555b0dbe
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-09-13 21:50:59 -0500

    - CHG: ToolBar Component has been improved

--- src/Common/Components/SideBarIcon.jsx
+++ src/Common/Components/SideBarIcon.jsx
@@ -1,6 +1,6 @@
 export const SidebarIcon = ({ Icon, text }) => (
-  <div className="flex items-center space-x-4 p-2 hover:bg-gray-800 rounded-full cursor-pointer group">
-    <Icon className="h-7 w-7 text-white group-hover:text-blue-400" />
+  <div className="flex items-center p-2 hover:bg-gray-800 rounded-full cursor-pointer group">
+    <Icon className="h-7 min-w-7 text-white group-hover:text-blue-400" />
     <span className="hidden xl:inline text-white group-hover:text-blue-400">
       {text}
     </span>
--- src/Home/Components/Toolbar.jsx
+++ src/Home/Components/Toolbar.jsx
@@ -1,38 +1,40 @@
 import {
-  FaTwitter,
-  FaHome,
-  FaHashtag,
-  FaBell,
-  FaEnvelope,
-  FaBookmark,
-  FaList,
-  FaUser,
-  FaEllipsisH,
-  FaFeather,
-} from 'react-icons/fa';
+  GoHomeFill,
+  GoSearch,
+  GoBell,
+  GoMail,
+  GoBookmark,
+  GoPerson,
+} from 'react-icons/go';
+import { BsSlashSquare, BsFeather } from 'react-icons/bs';
+import { PiDotsThreeCircleLight } from 'react-icons/pi';
+import {} from 'react-icons/bs';
+
+import { FaXTwitter } from 'react-icons/fa6';
 
 import { SidebarIcon } from './../../Common/Components';
 
 export const Toolbar = () => {
   return (
     <>
-      <div className="w-32 min-h-full p-2 bg-black">
+      <div className="flex flex-col justify-start items-center w-16 h-screen p-2 bg-black">
         <div className="p-2 hover:bg-gray-800 rounded-full cursor-pointer mb-4">
-          <FaTwitter className="h-8 w-8 text-blue-400" />
+          <FaXTwitter className="h-8 w-8 text-white" />
         </div>
 
-        <SidebarIcon Icon={FaHome} text="Home" />
-        <SidebarIcon Icon={FaHashtag} text="Explore" />
-        <SidebarIcon Icon={FaBell} text="Notifications" />
-        <SidebarIcon Icon={FaEnvelope} text="Messages" />
-        <SidebarIcon Icon={FaBookmark} text="Bookmarks" />
-        <SidebarIcon Icon={FaList} text="Lists" />
-        <SidebarIcon Icon={FaUser} text="Profile" />
-        <SidebarIcon Icon={FaEllipsisH} text="More" />
+        <div className="space-y-2">
+          <SidebarIcon Icon={GoHomeFill} />
+          <SidebarIcon Icon={GoSearch} />
+          <SidebarIcon Icon={GoBell} />
+          <SidebarIcon Icon={GoMail} />
+          <SidebarIcon Icon={GoBookmark} />
+          <SidebarIcon Icon={BsSlashSquare} />
+          <SidebarIcon Icon={GoPerson} />
+          <SidebarIcon Icon={PiDotsThreeCircleLight} />
+        </div>
 
-        <button className="bg-blue-400 text-white rounded-full text-lg font-bold shadow-lg hover:bg-blue-500 transition duration-200 mt-4 p-2">
-          <FaFeather className="h-6 w-6" />
-          <span className="hidden xl:inline">Tweet</span>
+        <button className="w-10 bg-blue-400 text-white rounded-full shadow-lg hover:bg-blue-500 transition duration-200 mt-4 p-2">
+          <BsFeather className="h-6 w-6" />
         </button>
       </div>
     </>
