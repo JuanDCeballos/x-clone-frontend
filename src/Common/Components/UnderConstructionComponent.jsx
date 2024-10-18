c47c0699912afb39e5ff53d5cf812c4a86bb93ab
Juan Ceballos <juandavi789@hotmail.com>
2024-10-18 18:33:29 -0500

    Update UnderConstruction component

--- src/Common/Components/UnderConstructionComponent.jsx
+++ src/Common/Components/UnderConstructionComponent.jsx
@@ -1,9 +1,9 @@
 export const UnderConstructionComponent = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center min-h-screen bg-[rgb(65,65,66)]">
-        <div className="w-1/5 bg-black rounded-3xl  flex flex-col items-center justify-center">
-          <img src="UnderConstruction.png" className="size-72" />
+      <div className="flex flex-col justify-center items-center min-h-screen bg-[rgb(65,65,66)] w-[634px]">
+        <div className="w-[634px] bg-black min-h-full flex flex-col items-center justify-center">
+          <img src="UnderConstruction.png" className="size-auto" />
           <div className="border-2 border-b-white rounded-full w-3/4 mb-4"></div>
           <p className="text-white font-extrabold text-xl">
             This site is not available
