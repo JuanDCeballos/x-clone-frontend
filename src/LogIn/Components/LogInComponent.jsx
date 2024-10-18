53225fb5516ebffcfe2a8262196a5bfa8797a580
Juan Ceballos <juandavi789@hotmail.com>
2024-10-18 18:32:14 -0500

    Update LogIn component

--- src/LogIn/Components/LogInComponent.jsx
+++ src/LogIn/Components/LogInComponent.jsx
@@ -10,29 +10,47 @@ export const LogInComponent = () => {
         <div className="w-2/4">
           <div className="w-full">
             <h3 className="font-extrabold text-white text-6xl">
-              Lo que está pasando ahora
+              Happening now
             </h3>
             <p className="mt-16 font-extrabold text-white text-3xl">
-              Únete hoy
+              Join today.
             </p>
           </div>
           <div>
-            <button className="bg-white mt-6 rounded-full w-2/4 h-11 flex flex-row items-center justify-center font-semibold hover:bg-gray-200">
-              <FaGoogle className="text-2xl mr-4" />
-              Regístrate con Google
-            </button>
-            <button className="bg-white mt-6 rounded-full w-2/4 h-11 flex flex-row items-center justify-center font-semibold hover:bg-gray-200">
-              <FaApple className="text-3xl mr-5" />
-              Regístrate con Apple
+            <div className="flex flex-col w-2/4 text-white gap-2 pt-2">
+              <label htmlFor="email-userName">Email or user name</label>
+              <input
+                type="text"
+                name="email"
+                id="email-userName"
+                placeholder="Email or user name"
+                className="border border-[rgb(29,155,240)] bg-black h-11 pl-2 focus:outline-none rounded"
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                placeholder="Password"
+                className="border border-[rgb(29,155,240)] bg-black h-11 pl-2 focus:outline-none rounded"
+              />
+            </div>
+            <button className="bg-[rgb(29,155,240)]  mt-6 rounded-full w-2/4 h-11 flex flex-row items-center justify-center font-semibold text-white hover:bg-[rgb(27,137,211)]">
+              Sign in
             </button>
           </div>
           <div className="mt-6 border-2 border-b-white rounded-full w-2/4"></div>
+          <div className="flex gap-1">
+            <button className="bg-white mt-6 rounded-full w-60 h-11 flex flex-row items-center justify-center font-semibold hover:bg-gray-200">
+              <FaGoogle className="text-2xl mr-4" />
+            </button>
+            <button className="bg-white mt-6 rounded-full w-60 h-11 flex flex-row items-center justify-center font-semibold hover:bg-gray-200">
+              <FaApple className="text-3xl mr-5" />
+            </button>
+          </div>
           <button className="bg-[rgb(29,155,240)]  mt-6 rounded-full w-2/4 h-11 flex flex-row items-center justify-center font-semibold text-white hover:bg-[rgb(27,137,211)]">
-            Crear cuenta
+            Create account
           </button>
-          <p className="mt-16 font-medium text-white text-lg cursor-pointer hover:text-gray-200">
-            ¿Ya tienes una cuenta?
-          </p>
         </div>
       </div>
     </>
