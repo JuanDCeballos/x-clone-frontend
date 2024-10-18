e8e2e13a6678b280e30c1e582f84344361675720
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-17 23:28:18 -0500

    - ADD: Component Trend has been created and set into AboutComponent

--- src/Home/Components/SearchBarComponent.jsx
+++ src/Home/Components/SearchBarComponent.jsx
@@ -3,11 +3,11 @@ import { IoSearch } from 'react-icons/io5';
 export const SearchBarComponent = () => {
   return (
     <>
-      <div className="w-[350px] ml-4 pt-2">
-        <div className="flex flex-row justify-start items-center bg-[rgb(47,50,53)] w-11/12 rounded-2xl">
+      <div className="w-full ml-4 pt-2">
+        <div className="flex flex-row justify-start items-center bg-[rgb(47,50,53)] w-11/12 rounded-full">
           <IoSearch className="text-white text-xl cursor-pointer ml-4" />
           <input
-            className="border border-[rgb(47,50,53)] bg-[rgb(47,50,53)] w-10/12 focus:outline-none text-white pl-4 h-8 font-semibold"
+            className="border border-[rgb(47,50,53)] bg-[rgb(47,50,53)] w-10/12 focus:outline-none text-white pl-4 h-12 font-semibold"
             placeholder="Search"
           />
         </div>
--- src/Home/Components/index.js
+++ src/Home/Components/index.js
@@ -2,3 +2,4 @@ export * from './AboutComponent';
 export * from './Feed';
 export * from './SearchBarComponent';
 export * from './Toolbar';
+export * from './TrendComponent';
