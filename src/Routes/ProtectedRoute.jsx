38f00a7902cd17741d0a6e067936d13d37eb3859
Juan Ceballos <juandavi789@hotmail.com>
2024-10-18 18:37:28 -0500

    Skeleton of private routing

---
+++ src/Routes/ProtectedRoute.jsx
@@ -0,0 +1,12 @@
+// import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  //   const authContext = useContext(AuthContext);
+
+  //   const { user or isLogged } = authContext;
+
+  //   return user ? children : <Navigate to="/login" />;
+  return null;
+};
+
+export default ProtectedRoute;
