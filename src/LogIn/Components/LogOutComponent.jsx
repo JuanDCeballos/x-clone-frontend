075a0df0cda6def7995053c8c89e62878d5d1156
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-21 20:41:36 -0500

    - CHG: Functionality to LogOut user in LogOutComponent has been set

--- src/LogIn/Components/LogOutComponent.jsx
+++ src/LogIn/Components/LogOutComponent.jsx
@@ -1,4 +1,25 @@
+import { useContext } from 'react';
+import { LogInContext } from '../Context/LogInContext';
+import { useNavigate } from 'react-router-dom';
+import { LogOutUser } from '../Controller';
+
 export const LogOutComponent = () => {
+  const { LogOut, User } = useContext(LogInContext);
+  const navigate = useNavigate();
+
+  const OnCancel = () => {
+    navigate('/feed', { replace: true });
+  };
+
+  const OnLogOut = async () => {
+    const result = await LogOutUser(User);
+
+    if (result.ok) {
+      LogOut();
+      navigate('/LogIn', { replace: true });
+    }
+  };
+
   return (
     <>
       <div className="min-h-screen bg-[rgb(65,65,66)] flex items-center justify-center">
@@ -14,10 +35,16 @@ export const LogOutComponent = () => {
               accounts, you can do that by adding an existing account.
             </p>
           </div>
-          <button className="bg-white mt-2 rounded-full w-3/4 h-8 font-semibold hover:bg-gray-200">
+          <button
+            className="bg-white mt-2 rounded-full w-3/4 h-8 font-semibold hover:bg-gray-200"
+            onClick={OnLogOut}
+          >
             Log out
           </button>
-          <button className="bg-black mt-4 mb-6 rounded-full w-3/4 h-8 border-solid border-2 border-gray-500 font-semibold hover:bg-gray-900 text-white">
+          <button
+            className="bg-black mt-4 mb-6 rounded-full w-3/4 h-8 border-solid border-2 border-gray-500 font-semibold hover:bg-gray-900 text-white"
+            onClick={OnCancel}
+          >
             Cancel
           </button>
         </div>
