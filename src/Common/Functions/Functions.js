73d857338730ae1d15119a51829bfef5b00a92a0
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-25 23:05:10 -0500

    - ADD: Function to get DateTime difference beetween today and Post createdAt value has been created

--- src/Common/Functions/Functions.js
+++ src/Common/Functions/Functions.js
@@ -2,3 +2,20 @@ export const getEnvironments = () => {
   import.meta.env;
   return { ...import.meta.env };
 };
+
+export function GetTimeDifference(dateTimeString) {
+  if(!dateTimeString)
+    return undefined;
+
+  const dateTime = new Date(dateTimeString);
+  const now = new Date();
+  const differenceInMs = now - dateTime;
+  const differenceInHours = Math.floor(differenceInMs / (1000 * 60 * 60));
+
+  if (differenceInHours >= 24) {
+    const differenceInDays = Math.floor(differenceInHours / 24);
+    return `${differenceInDays} ${differenceInDays === 1 ? "day" : "days"}`;
+  } else {
+    return `${differenceInHours} ${differenceInHours === 1 ? "hour" : "hours"}`;
+  }
+}
