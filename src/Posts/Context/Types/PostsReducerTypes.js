91277046a284303192f95d55e542083fa58a691e
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-29 00:08:40 -0500

    - CHG: ReducerTypes to PostsCreatedByUser has been created

--- src/Posts/Context/Types/PostsReducerTypes.js
+++ src/Posts/Context/Types/PostsReducerTypes.js
@@ -1,7 +1,9 @@
 export const PostReducerTypes = {
   LoadPosts: '[Posts] Load',
+  LoadPostsCreatedByUser: '[Posts] LoadPostsCreatedByUser',
   UpdateLastPostInfo: '[Posts] UpdateLastPostInfo',
   InsertCreatedPost: '[Posts] InsertCreatedPost',
   CloseTweetModal: '[Posts] CloseTweetModal',
   OpenTweetModal: '[Posts] OpenTweetModal',
+  UpdateLastPostInfoCreatedByUser: '[Posts] UpdateLastPostInfoCreatedByUser',
 };
