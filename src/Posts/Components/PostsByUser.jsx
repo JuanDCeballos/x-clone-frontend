024a22a868023371c3d19f54f44010ba4d4f19ec
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-29 00:09:01 -0500

    - ADD: Component PostsByUser has been created, it's not in its final implementation

---
+++ src/Posts/Components/PostsByUser.jsx
@@ -0,0 +1,44 @@
+import { Tweet, Post } from '../../Posts/Components';
+import { useContext, useEffect, useState } from 'react';
+import { PostsContext } from '../../Posts/Context/index.js';
+import { LogInContext } from '../../LogIn/Context/index.js';
+import { LoadingComponent } from '../../Common/Components';
+
+export const PostsByUser = () => {
+  const { GetPosts, posts } = useContext(PostsContext);
+  const { User, UserName } = useContext(LogInContext);
+  const [IsGettingData, SetIsGettingData] = useState(true);
+
+  useEffect(() => {
+    SetIsGettingData(true);
+    GetPosts(User).finally(() => {
+      SetIsGettingData(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="w-[634px]">
+        {IsGettingData ? (
+          <LoadingComponent />
+        ) : (
+          <>
+            <div className="border-l-2 border-r-2 flex text-center h-11  items-center justify-center">
+              <div className="flex-1 cursor-pointer">
+                <span>For you</span>
+              </div>
+              <div className="border-2 min-h-full rounded-b-full rounded-t-full border-gray-400"></div>
+              <div className="flex-1 cursor-pointer">
+                <span>Following</span>
+              </div>
+            </div>
+            <Tweet />
+            {posts.map((post) => (
+              <Post key={post._id} PostInfo={post} />
+            ))}
+          </>
+        )}
+      </div>
+    </>
+  );
+};
