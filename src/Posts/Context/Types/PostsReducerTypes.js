118080dc3da22e0ad4c8763bb9e92908aba4c61d
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-29 21:57:10 -0500

    - CHG: Types to manage posts created by UserName were added to types dictionary

--- src/Posts/Context/Types/PostsReducerTypes.js
+++ src/Posts/Context/Types/PostsReducerTypes.js
@@ -3,9 +3,14 @@ export const PostReducerTypes = {
   LoadPostsCreatedByUser: '[Posts] LoadPostsCreatedByUser',
   UpdateLastPostInfo: '[Posts] UpdateLastPostInfo',
   InsertCreatedPost: '[Posts] InsertCreatedPost',
+
   CloseTweetModal: '[Posts] CloseTweetModal',
   OpenTweetModal: '[Posts] OpenTweetModal',
+
   updateLastPostInfoFollowing: '[Posts] UpdateLastPostInfoFollowing',
   loadPostsFollowing: '[Posts] LoadPostsFollowing',
-  UpdateLastPostInfoCreatedByUser: '[Posts] UpdateLastPostInfoCreatedByUser',
+
+  UpdateLastPostInfoCreatedByUser:
+    '[Posts] UpdateLastPostInfoCreatedByUserName',
+  LoadPostsCreatedByUserName: '[Posts] LoadPostsCreatedByUserName',
 };
