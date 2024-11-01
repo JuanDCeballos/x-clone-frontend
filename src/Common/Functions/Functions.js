1cbce83de39893e3861eefdd1fefb1562a4042c0
Thomas-Parker24 <thomys65.ta@gmail.com>
2024-10-31 22:22:17 -0500

    - CHG: Function to Get Time Difference was modified to substract 5 hourt

--- src/Common/Functions/Functions.js
+++ src/Common/Functions/Functions.js
@@ -4,18 +4,20 @@ export const getEnvironments = () => {
 };
 
 export function GetTimeDifference(dateTimeString) {
-  if(!dateTimeString)
-    return undefined;
+  if (!dateTimeString) return undefined;
 
   const dateTime = new Date(dateTimeString);
   const now = new Date();
+  now.setHours(now.getHours() - 5);
   const differenceInMs = now - dateTime;
   const differenceInHours = Math.floor(differenceInMs / (1000 * 60 * 60));
 
   if (differenceInHours >= 24) {
     const differenceInDays = Math.floor(differenceInHours / 24);
-    return `${differenceInDays} ${differenceInDays === 1 ? "day" : "days"}`;
+    return `${differenceInDays} ${differenceInDays === 1 ? 'day' : 'days'}`;
+  } else if (differenceInHours == 0) {
+    return 'now';
   } else {
-    return `${differenceInHours} ${differenceInHours === 1 ? "hour" : "hours"}`;
+    return `${differenceInHours} ${differenceInHours === 1 ? 'hour' : 'hours'}`;
   }
 }
